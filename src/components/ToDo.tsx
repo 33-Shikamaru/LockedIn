d77a31db032542f5ce0307c67a8e2038e7012159
heartybp <parrenashearty@gmail.com>
2024-11-13 00:21:35 -0800

    Added components folder and To Do component

---
+++ src/components/ToDo.tsx
@@ -0,0 +1,11 @@
+export default function ToDo() {
+    return(
+        <div className="to-do-container">
+            <div className="search-bar">
+                <div className="to-do-list">
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
