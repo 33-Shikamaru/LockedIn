86928c7c267c467cfab4084fdcc5c0fcb7a28d63
33-Shikamaru <jeffersonmclinden@gmail.com>
2024-11-25 19:49:52 -0800

    dashnote contents centered

--- src/components/Dashnote.tsx
+++ src/components/Dashnote.tsx
@@ -12,7 +12,7 @@ type NameProp = {
 export default function DashNote({ username } :NameProp) {
     return(
         <div className="flex items-center justify-center">
-        <div className="flex space-x-2 w-1/2 bg-blue-100 rounded-lg px-5 py-5" aria-label="dashboard note">
+        <div className="flex justify-center space-x-2 w-1/2 bg-blue-100 rounded-lg px-5 py-5" aria-label="dashboard note">
             <p className='text-[#8EC2FF]'>Welcome to your Task Management Dashboard <span className="text-green-500 text-xl">{username}</span> ! </p>
             <DigitalClock />
         </div>
--- src/components/DigitalClock.tsx
+++ src/components/DigitalClock.tsx
@@ -18,7 +18,7 @@ export default function DigitalClock() {
         let hours = time.getHours();
         const minutes = time.getMinutes();
         const seconds = time.getSeconds();
-        const cycle = hours >= 13 ? " P" : " A";
+        const cycle = hours >= 13 ? "🌙" : "☀️";
 
         hours = hours % 12;
         return `${giveZero(hours)}:${giveZero(minutes)}:${giveZero(seconds)}${cycle}`;
@@ -30,7 +30,7 @@ export default function DigitalClock() {
     // flex justify-center items-center w-32 h-12
     return (
         <div className='flex items-center justify-center'>
-            <div className='font-digital text-2xl border-4 bg-[#E9F3FF] border-[#95BAE6] text-[#95BAE6] rounded-xl pt-3 pb-3 pr-5 pl-5'>
+            <div className='font-digital text-2xl border-4 bg-[#E9F3FF] border-[#95BAE6] text-[#95BAE6] rounded-xl pt-3 pb-3 pr-2 pl-2'>
                 <span>{formatTime()}</span>
             </div>
         </div>
