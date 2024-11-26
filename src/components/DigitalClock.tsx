df7885b27e73de340bf17a075d8e48e5fd980a6f
33-Shikamaru <jeffersonmclinden@gmail.com>
2024-11-25 20:54:32 -0800

    okay last last change to clock component...

--- src/components/DigitalClock.tsx
+++ src/components/DigitalClock.tsx
@@ -18,18 +18,18 @@ export default function DigitalClock() {
         let hours = time.getHours();
         const minutes = time.getMinutes();
         const seconds = time.getSeconds();
-        const cycle = hours >= 13 ? "🌙" : "☀️";
+        const prefix = hours >= 13 ? "🌙" : "☀️";
+        const cycle = hours >= 13 ? "P" : "A";
 
         hours = hours % 12;
-        return `${giveZero(hours)}:${giveZero(minutes)}:${giveZero(seconds)}${cycle}`;
+        return `${prefix} ${giveZero(hours)}:${giveZero(minutes)}:${giveZero(seconds)} ${cycle}`;
     }
 
     function giveZero(number : number) {
         return number < 10 ? "0" + number : number;
     } 
-    // flex justify-center items-center w-32 h-12
     return (
-        <div className='flex items-center justify-center'>
+        <div className='flex items-center justify-center text-center whitespace-nowrap'>
             <div className='font-digital text-2xl border-4 bg-[#E9F3FF] border-[#95BAE6] text-[#95BAE6] rounded-xl pt-3 pb-3 pr-2 pl-2'>
                 <span>{formatTime()}</span>
             </div>
