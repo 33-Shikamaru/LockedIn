ca9f002d35cc0387a00fd84e8dd0a1048f612d98
heartybp <parrenashearty@gmail.com>
2024-11-27 15:23:58 -0800

    fixed font

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
 import Login from './components/Login.tsx'
 import Signup from './components/SignUp.tsx'
 import Home from './components/Home.tsx'
