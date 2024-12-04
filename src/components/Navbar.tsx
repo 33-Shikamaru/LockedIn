8e60a14f41caef1ec8534e315e3cad67181eea9a
33-Shikamaru <jeffersonmclinden@gmail.com>
2024-12-03 20:14:06 -0800

    added icons to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,29 @@
+import HomeIcon from '@mui/icons-material/Home';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import StickyNote2Icon from '@mui/icons-material/StickyNote2';
+import SettingsIcon from '@mui/icons-material/Settings';
+import LogoutIcon from '@mui/icons-material/Logout';
+
 export default function Navbar() {
     return(
         <ul className="flex items-center justify-center p-5 text-[#8EC2FF]">
             <li className="px-6">
-                <a href="">Dashboard</a>
+                <a href=""><HomeIcon/>Dashboard</a>
             </li>
 
             <li className="px-6">
-                <a href="">Calendar</a>
+                <a href=""><CalendarMonthIcon/>Calendar</a>
             </li>
 
             <li className="px-6">
-                <a href="">Notes</a>
+                <a href=""><StickyNote2Icon/>Notes</a>
             </li>
 
             <li className="px-6">
-                <a href="">Settings</a>
+                <a href=""><SettingsIcon/>Settings</a>
+            </li>
+            <li>
+            <a href=""><LogoutIcon fontSize='medium'/>Logout</a>
             </li>
         </ul>
     )
